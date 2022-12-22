0cf1fb2da28684f0473dcdaedb58e18144904a5c
Undefined-deve1oper <kononovkirill05@gmail.com>
2022-12-22 19:49:00 +0600

    Trying to change the state in redux

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import "./styles/app.css";
+import { useDispatch, useSelector } from "react-redux";
 
 const App = () => {
+    const dispatch = useDispatch();
+    const cash = useSelector( state => state.cash );
+
+    const addCash = (cash) => {
+        dispatch({type: "ADD_CASH", payload: cash});
+    };
+    const getCash = (cash) => {
+        dispatch({type: "GET_CASH", payload: cash});
+    };
+
     return (
-        <div>
-            Hello redux
+        <div className="app">
+            <div style={ { fontSize: "60px" } }>{ cash }</div>
+            <div style={ { display: "flex" } }>
+                <button onClick={ () => addCash( Number( prompt() ) ) }>Пополнить счет</button>
+                <button onClick={ () => getCash( Number( prompt() ) ) }>Снять со счета</button>
+            </div>
         </div>
     );
 };
