b1f8f662a90d4c7bf5d92df8902a8aaecfa225fd
Undefined-deve1oper <kononovkirill05@gmail.com>
2022-12-22 21:54:03 +0600

    code refactor, creating a function for action

--- src/app/App.js
+++ src/app/App.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import "./styles/app.css";
 import { useDispatch, useSelector } from "react-redux";
+import { addCustomerAction, removeCustomerAction } from "./store/customerReducer";
+import { addCashAction, getCashAction } from "./store/cashReducer";
 
 const App = () => {
     const dispatch = useDispatch();
     const cash = useSelector( state => state.cash.cash );
+    const customers = useSelector( state => state.customers.customers );
 
-    const addCash = (cash) => {
-        dispatch({type: "ADD_CASH", payload: cash});
+    const addCash = ( cash ) => {
+        dispatch( addCashAction(cash) );
     };
-    const getCash = (cash) => {
-        dispatch({type: "GET_CASH", payload: cash});
+    const getCash = ( cash ) => {
+        dispatch( getCashAction(cash) );
+    };
+    const addCustomer = ( name ) => {
+        const customer = {
+            name,
+            id: Date.now(),
+        };
+        dispatch( addCustomerAction( customer ) );
+    };
+    const removeCustomer = ( customer ) => {
+        dispatch( removeCustomerAction( customer.id ) );
     };
 
     return (
@@ -19,7 +32,29 @@ const App = () => {
             <div style={ { display: "flex" } }>
                 <button onClick={ () => addCash( Number( prompt() ) ) }>Пополнить счет</button>
                 <button onClick={ () => getCash( Number( prompt() ) ) }>Снять со счета</button>
+                <button onClick={ () => addCustomer( prompt() ) }>Добавить клиента</button>
+                <button onClick={ () => getCash( Number( prompt() ) ) }>Удалить клиента</button>
             </div>
+            { customers.length > 0 ?
+                <div>
+                    { customers.map( customer =>
+                        <div style={ {
+                            fontSize: "2rem",
+                            border: "1px solid black",
+                            padding: "10px",
+                            marginTop: 5
+                        } }
+                             onClick={ () => removeCustomer( customer ) } key={ customer.id }
+                        >
+                            { customer.name }
+                        </div>
+                    ) }
+                </div>
+                :
+                <div style={ { fontSize: "2rem", marginTop: 20 } }>
+                    Клиенты отсутствуют!
+                </div>
+            }
         </div>
     );
 };
